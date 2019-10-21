31e67a993f417c34c59db146126bad1e9b4fe327
Samuel Macleod <samuel@macleod.space>
2019-10-22 00:47:59 +0100

    Better error handling

--- index.js
+++ index.js
@@ -11,51 +11,111 @@ addEventListener('fetch', event => {
 const r = new Router()
 if (define.get) {
     Object.keys(define.get).forEach(k => {
-        r.get(k, async (req, params) =>
-            (await define.get[k](req, params, {
-                status,
-                json,
-                text,
-                html,
-            })).toResponse()
-        )
+        r.get(k, async (req, params) => {
+            try {
+                return (await define.get[k](req, params, {
+                    status,
+                    json,
+                    text,
+                    html,
+                })).toResponse()
+            } catch (e) {
+                if (define.error) {
+                    return (await define.error(e, req, params, {
+                        status,
+                        json,
+                        text,
+                        html,
+                    })).toResponse()
+                } else {
+                    return status(500)
+                        .json({ error: e.toString() })
+                        .toResponse()
+                }
+            }
+        })
     })
 }
 if (define.post) {
     Object.keys(define.post).forEach(k => {
-        r.post(k, async (req, params) =>
-            (await define.post[k](req, params, {
-                status,
-                json,
-                text,
-                html,
-            })).toResponse()
-        )
+        r.post(k, async (req, params) => {
+            try {
+                return (await define.post[k](req, params, {
+                    status,
+                    json,
+                    text,
+                    html,
+                })).toResponse()
+            } catch (e) {
+                if (define.error) {
+                    return (await define.error(e, req, params, {
+                        status,
+                        json,
+                        text,
+                        html,
+                    })).toResponse()
+                } else {
+                    return status(500)
+                        .json({ error: e.toString() })
+                        .toResponse()
+                }
+            }
+        })
     })
 }
 
 if (define.put) {
     Object.keys(define.put).forEach(k => {
-        r.put(k, async (req, params) =>
-            (await define.put[k](req, params, {
-                status,
-                json,
-                text,
-                html,
-            })).toResponse()
-        )
+        r.put(k, async (req, params) => {
+            try {
+                return (await define.put[k](req, params, {
+                    status,
+                    json,
+                    text,
+                    html,
+                })).toResponse()
+            } catch (e) {
+                if (define.error) {
+                    return (await define.error(e, req, params, {
+                        status,
+                        json,
+                        text,
+                        html,
+                    })).toResponse()
+                } else {
+                    return status(500)
+                        .json({ error: e.toString() })
+                        .toResponse()
+                }
+            }
+        })
     })
 }
 if (define.delete) {
     Object.keys(define.delete).forEach(k => {
-        r.delete(k, async (req, params) =>
-            (await define.delete[k](req, params, {
-                status,
-                json,
-                text,
-                html,
-            })).toResponse()
-        )
+        r.delete(k, async (req, params) => {
+            try {
+                return (await define.delete[k](req, params, {
+                    status,
+                    json,
+                    text,
+                    html,
+                })).toResponse()
+            } catch (e) {
+                if (define.error) {
+                    return (await define.error(e, req, params, {
+                        status,
+                        json,
+                        text,
+                        html,
+                    })).toResponse()
+                } else {
+                    return status(500)
+                        .json({ error: e.toString() })
+                        .toResponse()
+                }
+            }
+        })
     })
 }
 async function handleRequest(request) {
